1d26c7d51138f1d61f49eed37cfa357543731027
AIEJamesBaker <s191482@students.aie.edu.au>
2022-03-29 08:12:58 +1030

    no message

--- Project3D/Application3D.cpp
+++ Project3D/Application3D.cpp
@@ -32,50 +32,12 @@ bool Application3D::startup() {
 		getWindowWidth() / (float)getWindowHeight(),
 		0.1f, 1000.f);
 
-
-	// Lighting colors
-	m_light.colour = { 1, 1, 0 };
-	m_ambientLight = { 0.25f, 0.25f, 0.25f };
-	m_light.direction = { -1.0, -1.0, -1.0 };
-
-	m_normalMapShader.loadShader(aie::eShaderStage::VERTEX,
-		"./shaders/normalmap.vert");
-	m_normalMapShader.loadShader(aie::eShaderStage::FRAGMENT,
-		"./shaders/normalmap.frag");
-	if (m_normalMapShader.link() == false) {
-		printf("Shader Error: %s\n", m_normalMapShader.getLastError());
-		return false;
-	}
-
-	if (m_spearMesh.load("./soulspear/soulspear.obj",
-		true, true) == false) {
-		printf("Soulspear Mesh Error!\n");
-		return false;
-	}
-
-	// Create scene
-	m_scene = new Scene(&m_camera, glm::vec2(getWindowWidth(),
-		getWindowHeight()), m_light, m_ambientLight);
-
-	// red light on the left
-	m_scene->getPointLights().push_back(Light(vec3(5, 3, 0), vec3(1, 1, 0), 50));
-	// green light on the right
-	m_scene->getPointLights().push_back(Light(vec3(-5, 3, 0), vec3(1, 1, 0), 50));
-
-	// Create 10 spears
-	for (int i = 0; i < 10; i++)
-	{
-		m_scene->addInstance(new Instance(glm::vec3(i, 1, 1), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1), &m_spearMesh,
-			&m_normalMapShader));
-	}
-
 	return true;
 }
 
 void Application3D::shutdown() {
 
 	Gizmos::destroy();
-	delete m_scene;
 }
 
 void Application3D::update(float deltaTime) 
@@ -100,20 +62,6 @@ void Application3D::update(float deltaTime)
 
 	m_camera.update(deltaTime);
 
-
-	ImGui::Begin("Light Settings");
-	ImGui::DragFloat3("Sunlight Direction", &m_scene->getLight().direction[0], 0.1f, -1.0f,
-		1.0f);
-	ImGui::DragFloat3("Sunlight Colour", &m_scene->getLight().colour[0], 0.1f, 0.0f,
-		2.0f);
-	ImGui::End();
-
-	ImGui::Begin("Scene Settings");
-	if (ImGui::Button("Spear Model - Normal Maps & Specular Highlights", ImVec2(400, 50)))
-	{
-
-	}
-	ImGui::End();
 	// quit if we press escape
 	aie::Input* input = aie::Input::getInstance();
 
@@ -129,8 +77,6 @@ void Application3D::draw() {
 	// update perspective in case window resized
 	m_projectionMatrix = m_camera.getProjectionMatrix(getWindowWidth(), getWindowHeight());
 	m_viewMatrix = m_camera.getViewMatrix();
-
-	m_scene->draw();
 	
 	// draw 3D gizmos
 	Gizmos::draw(m_projectionMatrix * m_viewMatrix);
